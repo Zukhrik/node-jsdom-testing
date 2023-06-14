e36b61082f8cbd4a16452856d1b0e4e8dcba8bdc
zukhrik <kz_spiky@mail.ru>
2023-06-14 16:15:20 +0500

    changed func

--- bundle.js
+++ bundle.js
@@ -2,12 +2,15 @@
 let data = {count: 0};
 
 const incrementCount = () => {
-  data.cheesecakes++;
-  window.document.getElementById("count").innerHTML = data.cheesecakes;
+  console.log('start');
+  data.count++
+  window.document.getElementById("count").innerHTML = data.count
+  console.log(data, 'end');
 };
 
 const incrementButton = window.document.getElementById("increment-button");
 incrementButton.addEventListener("click", incrementCount);
+
 
 module.exports = { incrementCount, data }
 },{}]},{},[1]);
