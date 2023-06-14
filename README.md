d024bc2f067e4a615470b37889fd82b2ab99a435
zukhrik <kz_spiky@mail.ru>
2023-06-14 13:07:46 +0500

    edited readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# node-jsdom-testing
+# Node-JSDOM-testing
+
+- node -v 16.17.1
+- jest -v 29.5.0
+- jsdom -v 22.1.0
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+
+  clearMocks: true,
+
+  collectCoverage: true,
+  
+  coverageDirectory: "coverage",
+
+  coverageProvider: "v8",
+  
+  testEnvironment: "jsdom",
+
+  verbose: true,
+};
