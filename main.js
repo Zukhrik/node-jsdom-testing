6382885d4d9733e2cffa8e5e7ffb4ecb621a9d4f
zukhrik <kz_spiky@mail.ru>
2023-06-14 15:13:44 +0500

    installing browserify

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let data = {count: 0};
 
 const incrementCount = () => {
   data.cheesecakes++;
-  window.document.getElementById("count").innerHTML = data.cheesecakes;
+  window.document.getElementById("count")
+    .innerHTML = data.cheesecakes;
 };
 
 const incrementButton = window.document.getElementById("increment-button");
